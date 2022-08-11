f99402708f9525cfdb770bd568175da19d736a0f
Md Irshad <irshadmd039@gmail.com>
2022-08-12 01:25:45 +0530

    playing with redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+
+const unsubscire = store.subscribe(()=>{
+    console.log("store changed", store.getState())
+})
+
+store.dispatch(bugAdded("new#Bug1"));
+store.dispatch(bugAdded("new#Bug2"));
+store.dispatch(bugAdded("new#Bug3"));
+store.dispatch(bugAdded("new#Bug4"));
+store.dispatch(bugRemoved(1));
+// unsubscire();
+store.dispatch(bugResolved(3));
+
+console.log(store.getState());
